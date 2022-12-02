e667dd382397622dc98f82577512c90b216ab4c2
David <davidalejandrorh@hotmail.com>
2022-12-01 20:49:29 -0500

    Se han corregido errores

--- src/main/java/com/david/auth/entities/Role.java
+++ src/main/java/com/david/auth/entities/Role.java
@@ -1,9 +1,6 @@
 package com.david.auth.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -11,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
-@Entity
+@Entity(name = "Role")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

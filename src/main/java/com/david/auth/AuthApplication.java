58a36248c7954eaa546d026d7b2e18dade5fb842
David <davidalejandrorh@hotmail.com>
2022-12-01 20:03:53 -0500

    Se configura prod

--- src/main/java/com/david/auth/AuthApplication.java
+++ src/main/java/com/david/auth/AuthApplication.java
@@ -1,9 +1,16 @@
 package com.david.auth;
 
 import com.david.auth.config.RsaKeyProperties;
+import com.david.auth.entities.Role;
+import com.david.auth.repo.RoleRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
@@ -13,4 +20,15 @@ public class AuthApplication {
         SpringApplication.run(AuthApplication.class, args);
     }
 
+    @Profile({"dev","prod"})
+    @Bean
+    CommandLineRunner run(RoleRepo roleRepo) {
+        return args -> {
+            List<Role> roles = roleRepo.findAll();
+            if(roles.isEmpty()) {
+                Role role = new Role("CLIENT");
+                roleRepo.save(role);
+            }
+        };
+    }
 }
